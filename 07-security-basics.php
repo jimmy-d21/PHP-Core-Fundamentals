73446036a579d032946dab13816698c1b0147550
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-09-19 23:41:45 +0800

    Password Hashing (Bcrypt/Argon2)

--- 07-security-basics.php
+++ 07-security-basics.php
@@ -5,4 +5,9 @@ $rawEmail = "   john.doe(at)example.com  ";
 $cleanEmail = filter_var(trim($rawEmail), FILTER_SANITIZE_EMAIL);
 echo $cleanEmail; // Result: john.doe(at)example.com
 
+// Password Hashing (Bcrypt/Argon2)
+$password = "SecretP@ss123";
+$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+echo "Hash generated"; // Result: Hash generated
+
 ?>
