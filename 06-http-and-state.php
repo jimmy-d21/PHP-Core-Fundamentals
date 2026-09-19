1f0be84c38b27d7f3a49b2af7ef141dc025f3492
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-09-19 23:40:43 +0800

    Form Handling ($_POST)

--- 06-http-and-state.php
+++ 06-http-and-state.php
@@ -9,4 +9,8 @@ echo "$method request from $clientIp"; // Result: GET request from 127.0.0.1
 $page = filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT) ?? 1;
 echo "Current Page: $page"; // Result: Current Page: 1
 
+// 29. Form Handling ($_POST)
+$username = $_POST['username'] ?? '';
+echo "Submitted user: " . htmlspecialchars($username);
+
 ?>
