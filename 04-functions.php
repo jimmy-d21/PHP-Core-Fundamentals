620013e2ea247810eb4d26bbea97a0908d8b80dd
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-09-19 23:37:53 +0800

    Named Arguments (PHP 8)

--- 04-functions.php
+++ 04-functions.php
@@ -6,4 +6,7 @@ function calculateTax(float $amount, float $rate = 0.12): float {
 }
 echo calculateTax(100.0); // Result: 12
 
+// 18. Named Arguments (PHP 8)
+echo calculateTax(rate: 0.15, amount: 200.0); // Result: 30
+
 ?>
