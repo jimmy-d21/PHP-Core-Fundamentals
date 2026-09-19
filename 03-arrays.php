2fdd91c9f055218466f02bc03e55070a5c444b2f
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-09-19 23:36:42 +0800

    Associative Arrays

--- 03-arrays.php
+++ 03-arrays.php
@@ -5,4 +5,15 @@ $colors = ["Red", "Green"];
 array_push($colors, "Blue");
 echo $colors[2]; // Result: Blue
 
+// 13. Associative Arrays
+$product = ["id" => 101, "name" => "Keyboard", "price" => 49.99];
+echo $product["name"]; // Result: Keyboard
+
+// 14. Multidimensional Arrays
+$matrix = [
+    [1, 2],
+    [3, 4]
+];
+echo $matrix[1][0]; // Result: 3
+
 ?>
